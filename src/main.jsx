44600d0f29fbe0301c9957134af1871e632683ae
fajar-setia <fajarsetiapambudi@gmail.com>
2026-09-20 23:57:45 +0700

    update: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // 👈 1. Ganti ke HashRouter
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/portofolio'>
+    {/* 👈 2. Gunakan HashRouter tanpa basename */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
