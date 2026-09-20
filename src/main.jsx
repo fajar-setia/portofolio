bd70a8f34d2c2d350cf058e1e195612e1a1c79ce
fajar-setia <fajarsetiapambudi@gmail.com>
2026-09-21 00:04:59 +0700

    fix: 404 pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom' // 👈 1. Ganti ke HashRouter
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 👈 2. Gunakan HashRouter tanpa basename */}
-    <HashRouter>
+    <BrowserRouter basename='/portofolio'> 
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
